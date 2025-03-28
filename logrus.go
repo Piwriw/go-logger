191a554b5589affbc0c59d5312dcc546321a4042
joohwan <piwriw@163.com>
2025-03-29 00:03:23 +0800

    fix logrus:Source func location

--- logrus.go
+++ logrus.go
@@ -2,19 +2,19 @@ package logger
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"io"
 	"os"
 	"path"
 	"runtime"
-
-	"github.com/sirupsen/logrus"
 )
 
-var defaultCallerPrettyfierFunc = func(f *runtime.Frame) (string, string) {
-	// 自定义文件名 + 行号格式
-	filename := path.Base(f.File)
-	return "", fmt.Sprintf("%s:%d", filename, f.Line)
-}
+//var defaultCallerPrettyfierFunc = func(f *runtime.Frame) (string, string) {
+//	// 自定义文件名 + 行号 + 函数名格式
+//	funcName := path.Base(f.Function)
+//	filename := path.Base(f.File)
+//	return fmt.Sprintf("%s()", funcName), fmt.Sprintf("%s:%d", filename, f.Line)
+//}
 
 // logrusLogger 实现
 
@@ -24,6 +24,7 @@ type logrusLogger struct {
 	level       Level
 	fields      logrus.Fields
 	colorScheme *ColorScheme
+	AddSource   bool
 }
 
 var _ Logger = (*logrusLogger)(nil)
@@ -31,28 +32,20 @@ var _ Logger = (*logrusLogger)(nil)
 func newLogrusLogger(opts Options) (Logger, error) {
 	logger := logrus.New()
 	errorLogger := logrus.New()
-	if opts.AddSource {
-		logger.SetReportCaller(true)
-		errorLogger.SetReportCaller(true)
-	}
 
 	if opts.JSONFormat {
 		logger.SetFormatter(&logrus.JSONFormatter{
-			CallerPrettyfier: defaultCallerPrettyfierFunc,
-			TimestampFormat:  opts.TimeFormat,
+			TimestampFormat: opts.TimeFormat,
 		})
 		errorLogger.Formatter = &logrus.TextFormatter{
-			CallerPrettyfier: defaultCallerPrettyfierFunc,
-			TimestampFormat:  opts.TimeFormat,
+			TimestampFormat: opts.TimeFormat,
 		}
 	} else {
 		logger.SetFormatter(&logrus.TextFormatter{
-			CallerPrettyfier: defaultCallerPrettyfierFunc,
-			TimestampFormat:  opts.TimeFormat,
+			TimestampFormat: opts.TimeFormat,
 		})
 		errorLogger.Formatter = &logrus.TextFormatter{
-			CallerPrettyfier: defaultCallerPrettyfierFunc,
-			TimestampFormat:  opts.TimeFormat,
+			TimestampFormat: opts.TimeFormat,
 		}
 	}
 	logger.SetLevel(ToLogrusLoggerLevel(opts.Level))
@@ -69,6 +62,9 @@ func newLogrusLogger(opts Options) (Logger, error) {
 	// 设置颜色输出
 	if opts.ColorEnabled {
 		logrusLogger.colorScheme = opts.ColorScheme
+	}
+	if opts.AddSource {
+		logrusLogger.AddSource = true
 	}
 	return logrusLogger, nil
 }
@@ -107,8 +103,14 @@ func FromLogrusLoggerLevel(level logrus.Level) Level {
 	}
 }
 
+func getCaller() string {
+	_, file, line, _ := runtime.Caller(3)
+	return fmt.Sprintf("%s:%d", path.Base(file), line)
+}
+
 func (l *logrusLogger) log(level logrus.Level, args ...any) {
 	msg := fmt.Sprint(args...)
+
 	if l.colorScheme != nil {
 		msg = l.colorScheme.Colorize(FromLogrusLoggerLevel(level), msg)
 	}
@@ -116,9 +118,19 @@ func (l *logrusLogger) log(level logrus.Level, args ...any) {
 		l.logger.WithFields(l.fields).Print(msg)
 		return
 	}
+
+	if l.AddSource {
+		l.logger.WithFields(logrus.Fields{
+			"source": getCaller(),
+		}).Log(level, msg)
+		return
+	}
+
 	l.logger.Log(level, msg)
 	if l.errorLogger != nil && level >= logrus.ErrorLevel {
-		l.errorLogger.Log(level, fmt.Sprint(msg))
+		l.errorLogger.WithFields(logrus.Fields{
+			"source": getCaller(),
+		}).Log(level, msg)
 	}
 }
 
